c5c7e1ab631f731ffd89856d561323653da78c01
Eduardo Jaime <eduardojaime86@gmail.com>
2021-10-05 18:58:37 -0400

    Hello world console app

---
+++ HelloWorldConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _net5practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
